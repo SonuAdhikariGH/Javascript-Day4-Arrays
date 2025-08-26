d1d61c38e35e55584d80fb598ef1f943cf8a63c3
Rohitdevatpy <227948873+SonuAdhikariGH@users.noreply.github.com>
2025-08-26 21:29:32 +0530

    Solved first task for arrays for making an array of favourite movies and printing the first and the last one

---
+++ overview.js
@@ -0,0 +1,8 @@
+// We are going to learn about arrays today and we will probably learn a lot about it in this session
+
+// Arrays basics - array is simply a container that holdsmultiple values
+
+// Exampe;
+// users = ["sonu", "rohit", "pranay"];
+
+// Topic 2 is string, string is simply a text that is either written in double quotes or single ones preferably
---
+++ task1.js
@@ -0,0 +1,7 @@
+// Create an array of 5 of your favorite movies and print the first and last movie.
+
+let favMovies = ["Spider Man", "Iron Man", "Avengers", "Super Man", "Bat Man"];
+
+// to do this question we first have to make a variable that will store all the movies name
+console.log("First Favourite:",favMovies[0] ,"||","Last Favourite:",favMovies[4]);
+// Then just console log all of them as shown above
