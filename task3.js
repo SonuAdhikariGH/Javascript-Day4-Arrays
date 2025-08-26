03759095c128018f2254b8fa1feb8cf026e9edea
Rohitdevatpy <227948873+SonuAdhikariGH@users.noreply.github.com>
2025-08-26 21:40:23 +0530

    Solved the third question and made the middle number printed using index number of that particular number asked in the question for arrays

---
+++ task3.js
@@ -0,0 +1,5 @@
+// From an array of numbers [10, 20, 30, 40, 50], print the middle number.
+let number  = [10, 20, 30, 40, 50];
+
+// to print the number that has been stated above we have to use the same process that we had in the first question by using index number of that specific number
+console.log(number[2]);
