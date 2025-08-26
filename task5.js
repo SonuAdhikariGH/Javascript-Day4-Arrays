f1a3d48116d02f102f39f2fbd51e1fda60eb11f0
Rohitdevatpy <227948873+SonuAdhikariGH@users.noreply.github.com>
2025-08-26 22:30:34 +0530

    solved the last questions of the list for array and strings

--- task5.js
+++ task5.js
@@ -1,1 +1,13 @@
-// Take let sentence = "I love JavaScript";
+let sentence = "I love JavaScript";
+
+// Converting the sentence to uppercase
+let upperCased = sentence.toUpperCase();
+console.log(upperCased);
+
+// Replace the word javascript with coding
+let replaced = sentence.replace("JavaScript", "Coding");
+console.log(replaced);
+
+// Split the sentence into words of forming an array
+let breakedSentences = sentence.split(" ");
+console.log(breakedSentences);
