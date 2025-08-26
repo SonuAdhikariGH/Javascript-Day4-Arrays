2aa933acb77c931618e8b53146d11df339220de2
Rohitdevatpy <227948873+SonuAdhikariGH@users.noreply.github.com>
2025-08-26 21:35:51 +0530

    I have solved the question for making name string container and calculate how many characters does my name has in strings

---
+++ tasj2.js
@@ -0,0 +1,7 @@
+// Make a string with your full name and print how many characters it has.
+
+const nameStr = "Sonu Adhikari";
+
+// To get the total count of character that my name has we have to use .length
+
+console.log(nameStr.length);
